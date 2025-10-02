6e13b31c336566dc4a0279afee9718772ab97b03
Joseph Olu <josepholuolofinte@gmail.com>
2025-10-02 16:51:41 +0100

    update. successfully connected the updateProfileForm to its api endpoint

--- src/api/UserApi.tsx
+++ src/api/UserApi.tsx
@@ -3,13 +3,13 @@ import { useMutation } from '@tanstack/react-query';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+// logic to commmunicate with createUser endpoint
 type CreateUserRequest = {
   auth0Id: string;
   email: string;
 };
 
 export const useCreateUser = () => {
-
   const { getAccessTokenSilently } = useAuth0();
 
   const createUserRequest = async (user: CreateUserRequest) => {
@@ -33,12 +33,62 @@ export const useCreateUser = () => {
     isPending,
     isError,
     isSuccess,
-  } = useMutation({mutationFn: createUserRequest});
+  } = useMutation({ mutationFn: createUserRequest });
 
   return {
     createUser,
     isPending,
     isError,
     isSuccess,
+  };
+};
+
+// logic to commmunicate with updateCurrentUser endpoint
+type UpdateCurrentuserRequestTypes = {
+  name: string;
+  address: string;
+  city: string;
+  country: string;
+};
+export const useUpdateCurrentUser = () => {
+  // get access token from auth0
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateCurrentUserRequest = async (
+    formData: UpdateCurrentuserRequestTypes
+  ) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/users`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to update user');
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutateAsync: updateCurrentUser,
+    isPending,
+    isError,
+    isSuccess,
+    error,
+    reset,
+  } = useMutation({ mutationFn: updateCurrentUserRequest });
+
+  return {
+    updateCurrentUser,
+    isPending,
+    isError,
+    isSuccess,
+    error,
+    reset,
   };
 };
--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,8 +1,11 @@
+import { useUpdateCurrentUser } from "../api/UserApi";
 import UserProfileForm from "../forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
+
+  const { updateCurrentUser, isPending } = useUpdateCurrentUser();
     return (
-      <UserProfileForm />
+      <UserProfileForm onSave={updateCurrentUser} isLoading={isPending} />
   )
 }
 
